3be78ec47a7b31628420380c8c7ed7284d7f8089
shithin-kp <shithinkp1999@gmail.com>
2025-07-22 09:50:46 +0530

    Added more pages and testcases

--- pages/attendance_page.py
+++ pages/attendance_page.py
@@ -6,3 +6,15 @@ class AttendancePage(BasePage):
     # Attendance page elements
     entry_tab = (By.XPATH, "//button[normalize-space(text())='Entry']")
     exit_tab = (By.XPATH, "//button[normalize-space(text())='Exit']")
+    exit_button=(By.XPATH, "//button[normalize-space(text())='Mark Exit']")
+
+    #Methods
+
+    def attendancePageEntryTabElements(self):
+        return self.driver.find_element(*self.entry_tab).is_displayed()
+
+    def attendancePageExitTabElements(self):
+        return self.driver.find_element(*self.exit_tab).is_displayed()
+
+    def clickMarkExit(self):
+        self.driver.find_element(*self.exit_button).click()
--- tests/test_04_attendance.py
+++ tests/test_04_attendance.py
@@ -22,4 +22,25 @@ def test_attendance_page_elements(driver):
     time.sleep(1)
     assert "home" in driver.current_url, "Failed to redirect to the home page after login"
     home_page.clickAttendance()
+    time.sleep(1)
+    assert attendance_page.attendancePageEntryTabElements() is True, "Entry tab is not displayed on the Attendance page"
+    assert attendance_page.attendancePageExitTabElements() is True, "Exit tab is not displayed on the Attendance page"
+
+@pytest.mark.priority2
+def test_mark_exit_button(driver):
+    """
+    Test description: Verify the functionality of the Mark Exit button on the Attendance page.
+    """
+    login_page = LoginPage(driver)
+    home_page = HomePage(driver)
+    attendance_page = AttendancePage(driver)
+    # Pause to let you solve the CAPTCHA manually
+    login_page.pauseForCaptcha()
+    login_page.logIn(username, password)
+    time.sleep(1)
+    assert "home" in driver.current_url, "Failed to redirect to the home page after login"
+    home_page.clickAttendance()
+    time.sleep(1)
+    attendance_page.clickMarkExit()
+    time.sleep(1)
 
